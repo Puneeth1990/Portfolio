1f8911087f91c4b529215f57cee66054a24ddab6
Puneeth1990 <puneeth.nie.s@gmail.com>
2017-08-16 12:02:36 -0500

    Made changes on UI

--- WebContent/app/js/App.js
+++ WebContent/app/js/App.js
@@ -1,8 +1,7 @@
-$(function() {
-    	   $("li").click(function() {
-    	      // remove classes from all
-    	      $("li").removeClass("active");
-    	      // add class to the one we clicked
-    	      $(this).addClass("active");
-    	   });
-    	});
+angular
+  .module('app', ['ngMessages'])
+  .controller('MainCtrl', MainCtrl);
+  
+function MainCtrl() {
+  
+}
